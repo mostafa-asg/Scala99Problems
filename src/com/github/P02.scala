7c768401a6b933363221bd6f8af6d692009340c8
asgari <asgari@inside.sahab.ir>
2016-05-10 11:02:18 +0430

    update problem 02 information

--- src/com/github/P02.scala
+++ src/com/github/P02.scala
@@ -2,10 +2,18 @@ package com.github
 
 /**
   *
-  * @author Mostafa Asgari 
+  * @author Mostafa Asgari
+  * Email : mostafa.asg@gmail.com
+  * website https://bigdatacode.wordpress.com/
   * @since 5/10/16
   *
-  **/
+  *
+  *    P02 (*) Find the last but one element of a list.
+  *    Example:
+  *    scala> penultimate(List(1, 1, 2, 3, 5, 8))
+  *   res0: Int = 5
+  *   
+  *   **/
 object P02 {
 
   def penultimate[T]( xs : List[T] ) : T = xs match {
