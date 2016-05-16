087db9a187826120518726020d95182c50c7b787
asgari <asgari@inside.sahab.ir>
2016-05-16 11:03:14 +0430

    Problem 33

---
+++ src/com/github/P33.scala
@@ -0,0 +1,19 @@
+package com.github
+
+import com.github.arithmetic.S99Int._
+
+/**
+  *
+  * @author mostafa 
+  * @since 5/16/16
+  */
+object P33 {
+
+  def main(args: Array[String]) {
+
+    println( 35.isCoprimeTo(64) )
+    println( 12.isCoprimeTo(18) )
+
+  }
+
+}
--- src/com/github/arithmetic/S99Int.scala
+++ src/com/github/arithmetic/S99Int.scala
@@ -7,9 +7,20 @@ package com.github.arithmetic
   */
 class S99Int(val num : Int) {
 
+  import S99Int._
+
   def isPrime() : Boolean = {
 
     !List.range(2, Math.sqrt(num).ceil.toInt+1).exists( n => num!=n && num%n==0 )
+
+  }
+
+  def isCoprimeTo(other : Int) : Boolean = {
+
+    gcd(num,other) match {
+      case 1 => true
+      case _ => false
+    }
 
   }
 
@@ -18,5 +29,16 @@ class S99Int(val num : Int) {
 object S99Int {
 
   implicit def intToS99Int(n : Int) : S99Int = new S99Int(n)
+
+  def gcd( a : Int , b : Int ) : Int = {
+
+    assert( a >= 0 )
+    assert( b >= 0 )
+
+    (a,b) match {
+      case (_,0) => a
+      case _ => gcd( b , a%b )
+    }
+  }
 
 }
