c780be92d080cd31c78436e22f368c2a830d7d5a
asgari <asgari@inside.sahab.ir>
2016-05-16 11:17:18 +0430

    update header information

--- src/com/github/P33.scala
+++ src/com/github/P33.scala
@@ -4,9 +4,17 @@ import com.github.arithmetic.S99Int._
 
 /**
   *
-  * @author mostafa 
+  * @author Mostafa Asgari
+  * Email : mostafa.asg@gmail.com
+  * website https://bigdatacode.wordpress.com/
   * @since 5/16/16
-  */
+  *
+  // P33 (*) Determine whether two positive integer numbers are coprime.
+  //     Two numbers are coprime if their greatest common divisor equals 1.
+  //
+  //     scala> 35.isCoprimeTo(64)
+  //     res0: Boolean = true
+  *     */
 object P33 {
 
   def main(args: Array[String]) {
--- src/com/github/P34.scala
+++ src/com/github/P34.scala
@@ -4,9 +4,19 @@ import com.github.arithmetic.S99Int._
 
 /**
   *
-  * @author mostafa 
+  * @author Mostafa Asgari
+  * Email : mostafa.asg@gmail.com
+  * website https://bigdatacode.wordpress.com/
   * @since 5/16/16
-  */
+  *
+  // P34 (**) Calculate Euler's totient function phi(m).
+  //     Euler's so-called totient function phi(m) is defined as the number of
+  //     positive integers r (1 <= r < m) that are coprime to m.  As a special
+  //     case, phi(1) is defined to be 1.
+  //
+  //     scala> 10.totient
+  //     res0: Int = 4
+  *    */
 object P34 {
 
   def main(args: Array[String]) {
