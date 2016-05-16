6c5fa79b1d8d8c89cb9165788ade4a437e12f418
asgari <asgari@inside.sahab.ir>
2016-05-16 10:55:56 +0430

    Problem 32

---
+++ src/com/github/P32.scala
@@ -0,0 +1,37 @@
+package com.github
+
+/**
+  *
+  * @author Mostafa Asgari
+  * Email : mostafa.asg@gmail.com
+  * website https://bigdatacode.wordpress.com/
+  * @since 5/16/16
+  *
+  // P32 (**) Determine the greatest common divisor of two positive integer
+  //          numbers.
+  //     Use Euclid's algorithm.
+  //
+  //     scala> gcd(36, 63)
+  //     res0: Int = 9
+  *    */
+object P32 {
+
+  def gcd( a : Int , b : Int ) : Int = {
+
+    assert( a >= 0 )
+    assert( b >= 0 )
+
+    (a,b) match {
+      case (_,0) => a
+      case _ => gcd( b , a%b )
+    }
+  }
+
+  def main(args: Array[String]) {
+
+    println( gcd(18,12) )
+    println( gcd(36,63) )
+
+  }
+
+}
